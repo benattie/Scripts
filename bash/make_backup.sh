77e89d93da70ec81d87c2f018781179f6f2f183a
benattie <benattie@localhost.localdomain>
2014-06-02 16:32:11 -0300

    Agregué el archivo extract_columns.
    Actualicé useful_commands.
    Hice un backup, así que tuve que modificar los archivos pertinentes.

--- bash/make_backup.sh
+++ bash/make_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "¿ Actualizaste /home/benattie/Templates/bash/rsync_exclude_list.txt ?"
-rsync -avz --progress --exclude-from '/home/benattie/Templates/bash/rsync_exclude_list.txt' /home/benattie/ /media/benattiHD_bkp/home_bkp/
+rsync -avz --progress --exclude-from '/home/benattie/Templates/bash/rsync_exclude_list.txt' /home/benattie/ /media/usb0/home_bkp/
