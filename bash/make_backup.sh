98cf93b5c4db47216368551ac7a68b947e755b65
benattie <emanuel.benatti@gmail.com>
2015-02-19 11:01:05 -0300

    Agregado archivos de licencia.
    Ademas combine este directorio con el directorio Scripts de Documents

--- bash/make_backup.sh
+++ bash/make_backup.sh
@@ -4,4 +4,4 @@ echo "Vaciando papelera"
 rm -Rf /home/benattie/.local/share/Trash/files/*
 rm -Rf /home/benattie/.local/share/Trash/info/*
 echo "¿ Actualizaste /home/benattie/Templates/bash/rsync_exclude_list.txt ?"
-rsync -avz --progress --exclude-from '/home/benattie/Templates/bash/rsync_exclude_list.txt' /home/benattie/ /media/benattiHD_bkp/home_bkp/
+rsync -avz --progress --exclude-from '/home/benattie/Templates/bash/rsync_exclude_list.txt' /home/benattie/ /media/benattie/benattiHD_bkp/home_bkp/
--- bash/youtube_download.sh
+++ bash/youtube_download.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # usage download.sh youtube_url
+cd ~/Downloads/
 ARGC=$#
 # echo $ARGC
-if [ $ARGC -eq 1 ]; then
-    youtube-dl --extract-audio $1
+if [ $ARGC -ge 1 ]; then
+    i=1
+    while [ $i -le $ARGC ]; do
+        youtube-dl --extract-audio ${!i}
+        i=$((i+1))
+    done
     #convert m4a to mp3
     
     FILES="*.m4a"
@@ -14,8 +19,11 @@ if [ $ARGC -eq 1 ]; then
         avconv -i "$F" -acodec libmp3lame -ab 192k "$newname.mp3"
     done
     rm *.m4a
+    echo "Done!"
 else
     echo "Numero incorrecto de argumentos"
     echo "Ejecute:"
     echo "youtube_download.sh youtube_url"
 fi
+
+mv *.mp3 ~/Music/
